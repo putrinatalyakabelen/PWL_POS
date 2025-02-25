f145dc82f2e26496b76b92c6b40c4384953fb000
putrinatalyakabelen <natalyakabelen2512@gmail.com>
2025-02-25 22:57:55 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use piv\View;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return View('home.inddex');
+    }
+}
---
+++ resources/views/user/show.blade.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+ <h1>Hello, Putri Kabelen </h1>
+ <h1>ID 08</h1>
+</body>
+</html>
