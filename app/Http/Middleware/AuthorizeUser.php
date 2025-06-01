bd82ad28a4f66996537513c91d54f6c9f22dcceb
putrinatalyakabelen <natalyakabelen2512@gmail.com>
2025-06-01 22:58:18 +0700

    PRAK 3 JS 7

--- app/Http/Middleware/AuthorizeUser.php
+++ app/Http/Middleware/AuthorizeUser.php
@@ -13,16 +13,13 @@ class AuthorizeUser
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, string $role = ''): Response
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        $user = $request->user(); // ambil data user yg login
-        // fungsi user() diambil dari UserMode.php
-        if ($user && $user->hasRole($role)) { // cek apakah user punya role yg diinginkan
-            return $next($request);
+        $user_role = $request->user()->getRole();
+ 
+        if (in_array($user_role, $roles)) {
+             return $next($request);
         }
-
-        // jika tidak punya role, maka tampilkan error 403
-        abort(403, 'Forbidden. Kamu tidak punya akses ke halaman ini!');
+        abort(403, 'Forbidden. Kamu tidak punya akses ke halaman ini.');
     }
-    
 }
