625369dc8b17e8c465ffdd0e5b9ac19ac9f86cb9
putrinatalyakabelen <natalyakabelen2512@gmail.com>
2025-06-01 21:16:55 +0700

    PRAK 2 JS 7

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -8,13 +8,8 @@ class Kernel extends HttpKernel
 {
     /**
      * The application's global HTTP middleware stack.
-     *
-     * These middleware are run during every request to your application.
-     *
-     * @var array<int, class-string|string>
      */
     protected $middleware = [
-        // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
@@ -25,8 +20,6 @@ class Kernel extends HttpKernel
 
     /**
      * The application's route middleware groups.
-     *
-     * @var array<string, array<int, class-string|string>>
      */
     protected $middlewareGroups = [
         'web' => [
@@ -39,7 +32,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
@@ -47,13 +39,10 @@ class Kernel extends HttpKernel
 
     /**
      * The application's middleware aliases.
-     *
-     * Aliases may be used instead of class names to conveniently assign middleware to routes and groups.
-     *
-     * @var array<string, class-string|string>
      */
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'authorize' => \App\Http\Middleware\AuthorizeUser::class, // middleware yg kita buat
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
--- routes/web.php
+++ routes/web.php
@@ -111,4 +111,21 @@ Route::pattern('id','[0-9]+'); // artinya ketika ada parameter {id}, maka harus 
  
     Route::get('/', [WelcomeController::class, 'index']);
  
+    //Route Level
+    Route::middleware(['authorize:ADM'])->group(function () {
+        Route::get('/', [LevelController::class, 'index']); // menampilkan halaman awal level
+        Route::post('/list', [LevelController::class, 'list']); // menampilkan data level dalam bentuk json untuk datatables
+        Route::get('/create', [LevelController::class, 'create']); // menampilkan halaman form tambah level
+        Route::post('/', [LevelController::class, 'store']); // menyimpan data level baru
+        Route::get('/create_ajax', [LevelController::class, 'create_ajax']); //Menampilkan halaman form tambah Level Ajax
+        Route::post('/ajax', [LevelController::class, 'store_ajax']); //Menyimpan data Level baru
+        Route::get('/{id}', [LevelController::class, 'show']); // menampilkan detail level
+        Route::get('/{id}/edit', [LevelController::class, 'edit']); // menampilkan halaman form edit level
+        Route::put('/{id}', [LevelController::class, 'update']); // menyimpan perubahan data level
+        Route::get('/{id}/edit_ajax', [LevelController::class, 'edit_ajax']); // menampilkan halaman form edit level Ajax
+        Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']); // menyimpan perubahan data level Ajax
+        Route::get('/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']); // Untuk tampilkan form confirm delete level Ajax
+        Route::delete('/{id}/delete_ajax', [LevelController::class, 'delete_ajax']); // menghapus data level Ajax
+        Route::delete('/{id}', [LevelController::class, 'destroy']); // menghapus data level
+    });
  });
